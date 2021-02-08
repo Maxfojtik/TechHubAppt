f7b3dfc7f2e50a28e559e3d99ac48f7ec271610b
Maxfojtik <maxfojtik@bex.net>
2021-02-08 13:50:08 -0500

    Aparently did a lot of work

--- TechHubAppt/src/Frame.java
+++ TechHubAppt/src/Frame.java
@@ -1,16 +1,461 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Locale;
+
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+import org.json.JSONException;
+
+import com.github.lgooddatepicker.components.DatePicker;
+import com.github.lgooddatepicker.components.DatePickerSettings;
+import com.github.lgooddatepicker.components.TimePickerSettings;
+import com.github.lgooddatepicker.components.TimePickerSettings.TimeArea;
+import com.github.lgooddatepicker.optionalusertools.DateChangeListener;
+import com.github.lgooddatepicker.zinternaltools.DateChangeEvent;
 
 public class Frame 
 {
 	static JFrame f;
 	static JButton newAppt;
 	static JButton deleteAppt;
-	static JButton printSchedule;
-	static JButton viewAppts;
+//	static JButton printSchedule;
+	static DatePicker datePicker;
+	static JButton nextDay;
+	static JButton prevDay;
+	static JLabel hourLabelLabel;
+	static JLabel nameLabel;
+	static JLabel emailLabel;
+	static JLabel noteLabel;
+	static LinkedList<JComponent> hourComps = new LinkedList<JComponent>();
+	static LinkedList<JTextArea> nameComps = new LinkedList<JTextArea>();
+	static LinkedList<JTextArea> emailComps = new LinkedList<JTextArea>();
+	static LinkedList<JTextArea> noteComps = new LinkedList<JTextArea>();
+	static LinkedList<Integer> unsavedAppts = new LinkedList<>();
+	static long lastUnsaved = 0;
+	static void save(Date day)
+	{
+		for(int i = 0; i < unsavedAppts.size(); i++) 
+		{
+			String name = nameComps.get(unsavedAppts.get(i)).getText();
+			String email = emailComps.get(unsavedAppts.get(i)).getText();
+			if(email.equals(""))
+			{
+				email = null;
+			}
+			String note = noteComps.get(unsavedAppts.get(i)).getText();
+			if(note.equals(""))
+			{
+				note = null;
+			}
+			Date date = indexToTime(unsavedAppts.get(i), day);
+			if(!name.trim().equals(""))
+			{
+				try {
+					TechHubAppt.saveAppt(new TechHubAppt.Appt(name, email, note, date));
+				} catch (JSONException | IOException | ParseException e) {
+					e.printStackTrace();
+				}
+			}
+			else
+			{
+				try {
+					TechHubAppt.deleteIfExists(date);
+				} catch (JSONException | IOException | ParseException e) {
+					e.printStackTrace();
+				}
+				nameComps.get(unsavedAppts.get(i)).setText("");
+				noteComps.get(unsavedAppts.get(i)).setText("");
+				emailComps.get(unsavedAppts.get(i)).setText("");
+			}
+			nameComps.get(unsavedAppts.get(i)).setEnabled(false);
+			noteComps.get(unsavedAppts.get(i)).setEnabled(false);
+			emailComps.get(unsavedAppts.get(i)).setEnabled(false);
+		}
+		f.setTitle("Tech Hub Appointments");
+		unsavedAppts.clear();
+		TechHubAppt.reHash();
+	}
+	static void logic()
+	{
+		for(int i = 0; i < Frame.nameComps.size(); i++)
+		{
+			JTextArea nameComp = Frame.nameComps.get(i);
+			JTextArea noteComp = Frame.noteComps.get(i);
+			JTextArea emailComp = Frame.emailComps.get(i);
+			nameComp.setBackground(Color.white);
+			noteComp.setBackground(Color.white);
+			emailComp.setBackground(Color.white);
+		}
+		if(datePicker.getDate().equals(LocalDate.now()))
+		{
+			int index = Frame.timeToIndex(new Date());
+			if(index>=0 && index<=15)
+			{
+				JTextArea nameComp = Frame.nameComps.get(index);
+				JTextArea noteComp = Frame.noteComps.get(index);
+				JTextArea emailComp = Frame.emailComps.get(index);
+				nameComp.setBackground(Color.lightGray);
+				noteComp.setBackground(Color.lightGray);
+				emailComp.setBackground(Color.lightGray);
+			}
+		}
+		long delta = System.currentTimeMillis()-lastUnsaved;
+		if(delta>0 && unsavedAppts.size()>0)
+		{
+			f.setTitle("Tech Hub Appointments (unsaved changes) Auto Saving in "+(60-delta/1000)+" seconds");
+			if(delta>60000)
+			{
+				save(localDateToDate(datePicker.getDate()));
+			}
+		}
+	}
+	static void checkAskUnsaved()
+	{
+		if(unsavedAppts.size()>0)
+		{
+			Object[] options = {"Save","Discard"};
+			int n = JOptionPane.showOptionDialog(null,
+			    "Would you like to save these?",
+			    "You have unsaved changes",
+			    JOptionPane.YES_NO_OPTION,
+			    JOptionPane.QUESTION_MESSAGE,
+			    null,
+			    options,
+			    options[0]);
+			if(n==0)
+			{
+				save(localDateToDate(datePicker.getDate()));
+			}
+			else
+			{
+				unsavedAppts.clear();
+				f.setTitle("Tech Hub Appointments");
+			}
+		}
+	}
 	static void init()
 	{
 		f = new JFrame("Tech Hub Appointments");
-		f.setBounds(10,10,800,600);
+		f.setBounds(10,10,800,450);
+		f.setResizable(false);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setLayout(null);
+		f.addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+				checkAskUnsaved();
+			}
+        });
+		hourLabelLabel = new JLabel("Time");
+		int yStart = 50;
+		int deltaY = 20;
+		hourLabelLabel.setBounds(10, yStart-deltaY, 100, deltaY);
+		f.add(hourLabelLabel);
+		nameLabel = new JLabel("Name");
+		nameLabel.setBounds(110, yStart-deltaY, 150, deltaY);
+		f.add(nameLabel);
+		emailLabel = new JLabel("Name.Number (unused)");
+		emailLabel.setBounds(270, yStart-deltaY, 150, deltaY);
+		f.add(emailLabel);
+		noteLabel = new JLabel("Note (opt)");
+		noteLabel.setBounds(430, yStart-deltaY, 250, deltaY);
+		f.add(noteLabel);
+		nextDay = new JButton("Next Day");
+		int frameWidth = 785;
+		nextDay.setBounds(frameWidth-155, 5, 150, 25);
+		nextDay.addActionListener(new NextDayListener());
+		f.add(nextDay);
+		prevDay = new JButton("Previous Day");
+		prevDay.setBounds(5, 5, 150, 25);
+		prevDay.addActionListener(new PrevDayListener());
+		f.add(prevDay);
+		datePicker = new DatePicker();
+		datePicker.setDateToToday();
+		int widthOfChoose = 160;
+		datePicker.setBounds(frameWidth/2-widthOfChoose/2, 5, widthOfChoose, 25);
+		datePicker.getComponentDateTextField().setEditable(false);
+		datePicker.addDateChangeListener(new DateChangeListener() 
+		{	
+			@Override
+			public void dateChanged(DateChangeEvent event) 
+			{
+				if(unsavedAppts.size()>0)
+				{
+					checkAskUnsaved();
+//					unsavedAppts.clear();
+//					f.setTitle("Tech Hub Appointments (discarded old data)");
+				}
+				showDay(TechHubAppt.getAppts(), event.getNewDate());
+			}
+		});
+		f.add(datePicker);
+		for(double i = 13; i < 16.9; i = i + 0.25)
+		{
+			int hour = (int)i;
+			int minutes = (int) ((i-hour)*60);
+			String minutesString = minutes!=0 ? String.valueOf(minutes) : "00";
+			JLabel hourLabel = new JLabel((hour-12)+":"+minutesString +" PM");
+			int line = (int)(yStart + (i-13)*(deltaY*4+10));
+			hourLabel.setBounds(10, line, 100, deltaY);
+			hourComps.add(hourLabel);
+			f.add(hourLabel);
+			
+			JTextArea nameTA = new JTextArea(1,1);
+			nameTA.getDocument().putProperty("filterNewlines", Boolean.TRUE);
+			nameTA.setBounds(110, line, 150, deltaY);
+			nameTA.addKeyListener(new ChangeListener(nameComps.size()));
+			nameTA.addMouseListener(new ClickMouseListener());
+			nameTA.setDisabledTextColor(Color.BLUE);
+			f.add(nameTA);
+			nameComps.add(nameTA);
+			
+			JTextArea emailTA = new JTextArea(1,1);
+			emailTA.getDocument().putProperty("filterNewlines", Boolean.TRUE);
+			emailTA.setBounds(270, line, 150, deltaY);
+			emailTA.addKeyListener(new ChangeListener(emailComps.size()));
+			emailTA.setDisabledTextColor(Color.BLUE);
+			emailTA.addMouseListener(new ClickMouseListener());
+			f.add(emailTA);
+			emailComps.add(emailTA);
+			
+			JTextArea noteTA = new JTextArea(1,1);
+			noteTA.getDocument().putProperty("filterNewlines", Boolean.TRUE);
+			noteTA.setBounds(430, line, 350, deltaY);
+			noteTA.addKeyListener(new ChangeListener(noteComps.size()));
+			noteTA.setDisabledTextColor(Color.BLUE);
+			noteTA.addMouseListener(new ClickMouseListener());
+			f.add(noteTA);
+			noteComps.add(noteTA);
+		}
+		showDay(TechHubAppt.getAppts(), datePicker.getDate());
+		f.setVisible(true);
+	}
+	static int timeToIndex(Date d)
+	{
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		int index = (int) (((cal.get(Calendar.HOUR_OF_DAY)-13)*4+cal.get(Calendar.MINUTE)/15));
+		return index;
+	}
+	static Date indexToTime(int index, Date date)
+	{
+		int hour = index/4+13;
+		int minute = index%4*15;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+//		cal.set(Calendar.YEAR, date.getYear());
+//		cal.set(Calendar.DAY_OF_YEAR, date.getDayOfYear());
+		cal.set(Calendar.HOUR_OF_DAY, hour);
+		cal.set(Calendar.MINUTE, minute);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		System.out.println(hour+"\t"+minute);
+		return cal.getTime();
+	}
+	static void showDay(LinkedList<TechHubAppt.Appt> appts, LocalDate dayDate)
+	{
+		logic();
+		for(int i = 0; i < nameComps.size(); i++)
+		{
+			nameComps.get(i).setText("");
+			emailComps.get(i).setText("");
+			noteComps.get(i).setText("");
+			nameComps.get(i).setEnabled(false);
+			emailComps.get(i).setEnabled(false);
+			noteComps.get(i).setEnabled(false);
+		}
+		if(dayDate!=null && appts!=null)
+		{
+			LinkedList<TechHubAppt.Appt> drawingAppts = new LinkedList<>();
+			Calendar cal = Calendar.getInstance();
+			for(int i = 0; i < appts.size(); i++)
+			{
+				cal.setTime(appts.get(i).date);
+				int day = cal.get(Calendar.DAY_OF_YEAR);
+				int year = cal.get(Calendar.YEAR);
+				int thisDay = dayDate.getDayOfYear();
+				int thisYear = dayDate.getYear();
+				if(day == thisDay && year == thisYear)
+				{
+					drawingAppts.add(appts.get(i));
+				}
+			}
+			for(int i = 0; i < drawingAppts.size(); i++)
+			{
+				TechHubAppt.Appt thisAppt = drawingAppts.get(i);
+				int index = timeToIndex(thisAppt.date);
+				nameComps.get(index).setText(thisAppt.name);
+				emailComps.get(index).setText(thisAppt.email);
+				noteComps.get(index).setText(thisAppt.note);
+			}
+		}
+	}
+	static void unsavedChanges(int index)
+	{
+		lastUnsaved = System.currentTimeMillis();
+		if(!unsavedAppts.contains(index))
+		{
+			unsavedAppts.add(index);
+			f.setTitle("Tech Hub Appointments (unsaved changes)");
+		}
+	}
+	static void showPrevDay()
+	{
+		checkAskUnsaved();
+		datePicker.setDate(getPrevDay(datePicker.getDate()));
+	}
+	static void showNextDay()
+	{
+		checkAskUnsaved();
+		datePicker.setDate(getNextDay(datePicker.getDate()));
+	}
+	static LocalDate getNextDay(LocalDate old)
+	{
+		while(true)
+		{
+	        old = old.plusDays(1);
+	        if (old.getDayOfWeek() != DayOfWeek.SATURDAY && old.getDayOfWeek() != DayOfWeek.SUNDAY) 
+	        {
+	        	break;
+	        }
+		}
+        return old;
+	}
+	static LocalDate getPrevDay(LocalDate old)
+	{
+		while(true)
+		{
+	        old = old.minusDays(1);
+	        if (old.getDayOfWeek() != DayOfWeek.SATURDAY && old.getDayOfWeek() != DayOfWeek.SUNDAY) 
+	        {
+	        	break;
+	        }
+		}
+        return old;
+	}
+	static class NextDayListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			showNextDay();
+		}
+	}
+	static class PrevDayListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			showPrevDay();
+		}
+	}
+	static class ClickMouseListener implements java.awt.event.MouseListener
+	{
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			e.getComponent().setEnabled(true);
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			
+		}
+		
+	}
+	static Date localDateToDate(LocalDate d)
+	{
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, d.getYear());
+		cal.set(Calendar.DAY_OF_YEAR, d.getDayOfYear());
+		cal.set(Calendar.HOUR, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		return cal.getTime();
+	}
+	static class ChangeListener implements KeyListener
+	{
+		int index;
+		public ChangeListener(int index) {
+			this.index = index;
+		}
+		@Override
+		public void keyPressed(KeyEvent e) 
+		{
+			if(e.getKeyChar()=='\n')
+			{
+				save(localDateToDate(datePicker.getDate()));
+			}
+			else if(e.getKeyChar()=='\t')
+			{
+				if(nameComps.contains(e.getComponent()))
+				{
+					int index = nameComps.indexOf(e.getComponent());
+					JTextArea ta = emailComps.get(index);
+					ta.setEnabled(true);
+					ta.requestFocus();
+				}
+				else if(emailComps.contains(e.getComponent()))
+				{
+					int index = emailComps.indexOf(e.getComponent());
+					JTextArea ta = noteComps.get(index);
+					ta.setEnabled(true);
+					ta.requestFocus();
+				}
+				e.consume();
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) 
+		{
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) 
+		{
+			unsavedChanges(index);
+		}
 	}
 }
--- TechHubAppt/src/TechHubAppt.java
+++ TechHubAppt/src/TechHubAppt.java
@@ -1,14 +1,29 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.Locale;
+import static java.nio.file.StandardWatchEventKinds.*;
+import javax.swing.JTextArea;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,63 +43,230 @@ public class TechHubAppt
 	}
 	static String readFile(String path) throws IOException
 	{
-		byte[] encoded = Files.readAllBytes(Paths.get(path));
-		return new String(encoded);
+		try
+		{
+			byte[] encoded = Files.readAllBytes(Paths.get(path));
+			return new String(encoded);
+		}
+		catch(Exception e)
+		{
+			return null;
+		}
 	}
+	static FileOutputStream lockFileOutputStream;
+	static FileLock lock;
 	static LinkedList<Appt> loadAppts(String path) throws IOException, JSONException, ParseException
 	{
 		String rawJSON = readFile(path);
-		JSONArray jsonArray = new JSONArray(rawJSON);
 		LinkedList<Appt> tempList = new LinkedList<Appt>();
-		for(int i = 0; i < jsonArray.length(); i++)
+		if(rawJSON!=null)
 		{
-			JSONObject apptObj = jsonArray.getJSONObject(i);
-			String name = apptObj.getString("name");
-			String email = null;
-			if(apptObj.has("email"))
+			JSONArray jsonArray = new JSONArray(rawJSON);
+			for(int i = 0; i < jsonArray.length(); i++)
 			{
-				email = apptObj.getString("email");
+				JSONObject apptObj = jsonArray.getJSONObject(i);
+				String name = apptObj.getString("name");
+				String email = null;
+				if(apptObj.has("email"))
+				{
+					email = apptObj.getString("email");
+				}
+				String note = null;
+				if(apptObj.has("note"))
+				{
+					note = apptObj.getString("note");
+				}
+	//			DateFormat df = new 
+				Date date = df.parse(apptObj.getString("date"));
+				Appt tempAppt = new Appt(name, email, note, date);
+				tempList.add(tempAppt);
 			}
-			String note = null;
-			if(apptObj.has("note"))
-			{
-				note = apptObj.getString("note");
-			}
-//			DateFormat df = new 
-			Date date = df.parse(apptObj.getString("date"));
-			Appt tempAppt = new Appt(name, email, note, date);
-			tempList.add(tempAppt);
 		}
 		return tempList;
 	}
+	static void saveAppt(Appt appt) throws JSONException, IOException, ParseException
+	{
+		saveAppt(myPath, appt);
+	}
+	static LinkedList<Appt> deleteIfExists(LinkedList<Appt> in, Date date)
+	{
+		for(int i = 0; i < in.size(); i++)
+		{
+			if(in.get(i).date.equals(date))
+			{
+				System.out.println("Deleted Appt at "+date);
+				in.remove(i);
+				i--;
+			}
+		}
+		return in;
+	}
+	static void deleteIfExists(Date date) throws IOException, JSONException, ParseException
+	{
+		LinkedList<Appt> tempList = loadAppts(myPath);
+		deleteIfExists(tempList, date);
+		JSONArray output = new JSONArray();
+		for(int i = 0; i < tempList.size(); i++)
+		{
+			JSONObject apptObj = new JSONObject();
+			Appt thisAppt = tempList.get(i);
+			apptObj.put("name", thisAppt.name);
+			if(thisAppt.email!=null)
+			{
+				apptObj.put("email", thisAppt.email);
+			}
+			if(thisAppt.note!=null)
+			{
+				apptObj.put("note", thisAppt.note);
+			}
+			apptObj.put("date", df.format(thisAppt.date));
+			output.put(apptObj);
+		}
+		FileWriter file = new FileWriter(myPath);
+        file.write(output.toString());
+        file.close();
+        System.out.println("Saved "+output.length()+" appts");
+	}
 	static void saveAppt(String path, Appt appt) throws IOException, JSONException, ParseException
 	{
-		String rawJSON = readFile(path);
-		JSONArray jsonArray = new JSONArray(rawJSON);
-		LinkedList<Appt> tempList = new LinkedList<Appt>();
-		for(int i = 0; i < jsonArray.length(); i++)
+		LinkedList<Appt> tempList = loadAppts(path);
+		deleteIfExists(tempList, appt.date);
+		tempList.add(appt);
+		JSONArray output = new JSONArray();
+		for(int i = 0; i < tempList.size(); i++)
 		{
-			JSONObject apptObj = jsonArray.getJSONObject(i);
-			String name = apptObj.getString("name");
-			String email = null;
-			if(apptObj.has("email"))
+			JSONObject apptObj = new JSONObject();
+			Appt thisAppt = tempList.get(i);
+			apptObj.put("name", thisAppt.name);
+			if(thisAppt.email!=null)
 			{
-				email = apptObj.getString("email");
+				apptObj.put("email", thisAppt.email);
 			}
-			String note = null;
-			if(apptObj.has("note"))
+			if(thisAppt.note!=null)
 			{
-				note = apptObj.getString("note");
+				apptObj.put("note", thisAppt.note);
 			}
-			Date date = df.parse(apptObj.getString("date"));
-			Appt tempAppt = new Appt(name, email, note, date);
-			tempList.add(tempAppt);
+			apptObj.put("date", df.format(thisAppt.date));
+			output.put(apptObj);
 		}
+		FileWriter file = new FileWriter(path);
+        file.write(output.toString());
+        file.close();
+        System.out.println("Saved "+output.length()+" appts");
 	}
-	public static void main(String args[]) throws URISyntaxException
+	static String oldHash;
+	static boolean needReload()
 	{
-		df = new SimpleDateFormat("MM/dd/YYYY HH:mm", Locale.ENGLISH);
-		myPath = new File(TechHubAppt.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+		String newHash = hash(new File("./appts.json"));
+		System.out.println(newHash+":"+oldHash);
+		return !newHash.equals(oldHash);
+	}
+	static void reHash()
+	{
+		oldHash = hash(new File("./appts.json"));
+	}
+	static boolean fileChanged(WatchKey key, WatchService watcher)
+	{
+		key = watcher.poll();
+		if(key!=null)
+		{
+		    for (WatchEvent<?> event: key.pollEvents()) {
+		        WatchEvent.Kind<?> kind = event.kind();
+		        if (kind == OVERFLOW) {
+		            continue;
+		        }
+		        final Path changed = (Path) event.context();
+	            System.out.println(changed);
+	            if (changed.endsWith("appts.json")) {
+	                return true;
+	            }
+		    }
+		    boolean valid = key.reset();
+		    if (!valid) {
+		        return false;
+		    }
+		}
+		return false;
+	}
+	public static void main(String args[]) throws URISyntaxException, JSONException, IOException, ParseException, InterruptedException
+	{
+		File folder = new File("./Users");
+		folder.mkdir();
+		File file = new File("./Users/"+System.getProperty("user.name")+".txt");
+		file.createNewFile();
+        file.deleteOnExit();
+		df = new SimpleDateFormat("HH:mm MM/dd/yyyy", Locale.ENGLISH);
+		myPath = new File("./appts.json").getPath();
+		Frame.init();
+		long loopNum = 0;
+		reHash();
+		WatchService watcher = FileSystems.getDefault().newWatchService();
+		Path dir = Paths.get("./");
+		WatchKey key = null;
+		try {
+		    key = dir.register(watcher, ENTRY_MODIFY);
+		} catch (IOException x) {
+		    System.err.println(x);
+		    System.exit(1);
+		}
+		while(true)
+		{
+			if(loopNum++%100==0 || fileChanged(key, watcher))
+			{
+				if(needReload() && Frame.unsavedAppts.size()==0)
+				{
+					System.out.println("Needs reload");
+					Frame.showDay(TechHubAppt.getAppts(), Frame.datePicker.getDate());
+					oldHash = hash(new File("./appts.json"));
+				}
+			}
+			Frame.logic();
+			File f = new File("./Users/plsclose.txt");
+			if(f.exists())
+			{
+				System.exit(0);
+			}
+			Thread.sleep(100);
+		}
+//		Frame.showDay(appts);
+//		saveAppt(myPath, new Appt("Max", null, null, new Date()));
+	}
+
+	static String hash(File f)
+	{
+		try {
+			FileInputStream fis = new FileInputStream(f);
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			//Create byte array to read data in chunks
+		    byte[] byteArray = new byte[1024];
+		    int bytesCount = 0; 
+		      
+		    //Read file data and update in message digest
+		    while ((bytesCount = fis.read(byteArray)) != -1) {
+		    	digest.update(byteArray, 0, bytesCount);
+		    };
+		     
+		    //close the stream; We don't need it now.
+		    fis.close();
+			byte[] hash = digest.digest();
+			//This bytes[] has bytes in decimal format;
+		    //Convert it to hexadecimal format
+		    StringBuilder sb = new StringBuilder();
+		    for(int i=0; i< hash.length ;i++)
+		    {
+		        sb.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
+		    }
+		     
+		    //return complete hash
+		   return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+		}
+		return "";
 	}
 	static class Appt
 	{
